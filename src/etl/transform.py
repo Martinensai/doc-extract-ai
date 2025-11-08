2c9101b84fea05396cdbdaaf8e9b63721f6a07b6
Josephson Junior <josephson-junior.razafindratsiza@eleve.ensai.fr>
2025-11-08 15:53:06 +0100

    test dag_try complete

--- src/etl/transform.py
+++ src/etl/transform.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 # --- Configuration (A exécuter une seule fois au chargement du module) ---
 load_dotenv()
-API_KEY = os.getenv("GEMINI_API_KEY")
+API_KEY = os.getenv("api_key")
 
 # Vérification de la clé API
 if not API_KEY:
